04007758932cefec76b381b10c8e07ab07dba81b
Joe Burgess <bojurgess@gmail.com>
2024-05-07 13:55:50 +0100

    idk

--- src/commands/utility/ping.ts
+++ src/commands/utility/ping.ts
@@ -2,7 +2,6 @@ import { SlashCommandBuilder } from 'discord.js';
 import type { CommandInteraction } from 'discord.js';
 
 export default {
-    cooldown: 3,
     data: new SlashCommandBuilder()
         .setName('ping')
         .setDescription('Replies with Pong!'),
